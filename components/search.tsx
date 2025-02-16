d017b029d3b3477beca49f09278e9305d3754962
Marty <riedlmarty@gmail.com>
2025-02-16 23:24:23 +0100

    Update Search component styles and placeholder text for improved localization and consistency

--- components/search.tsx
+++ components/search.tsx
@@ -6,7 +6,7 @@ import icons from "@/constants/icons";
 import { useLocalSearchParams, router, usePathname } from "expo-router";
 
 const Search = () => {
-  const path = usePathname();
+  // const path = usePathname();
   const params = useLocalSearchParams<{ query?: string }>();
   const [search, setSearch] = useState(params.query);
 
@@ -20,14 +20,14 @@ const Search = () => {
   };
 
   return (
-    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-white border border-primary-500 mt-5 py-2">
       <View className="flex-1 flex flex-row items-center justify-start z-50">
         <Image source={icons.search} className="size-5" />
         <TextInput
           value={search}
           onChangeText={handleSearch}
-          placeholder="Search for anything"
-          className="text-sm font-rubik text-black-300 ml-2 flex-1"
+          placeholder="Hledat cokoliv"
+          className="text-sm font-quicksand-medium text-black ml-2 flex-1"
         />
       </View>
 
