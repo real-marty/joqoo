5bb52a73366465e60d18b7095c234bf5256bc83e
Marty <riedlmarty@gmail.com>
2025-02-16 21:10:14 +0100

    Add AppLayout component to manage loading state and redirect unauthenticated users

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,22 @@
+import { useGlobalContext } from "@/context/global-context";
+import { Redirect, Slot } from "expo-router";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AppLayout() {
+  const { loading, isLoggedIn } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <SafeAreaView className="bg-white h-full justify-center items-center">
+        <ActivityIndicator className="text-primary-500" size={"large"} />
+      </SafeAreaView>
+    );
+  }
+
+  if (!isLoggedIn) {
+    return <Redirect href={"/sign-log-in"} />;
+  }
+
+  return <Slot />;
+}
