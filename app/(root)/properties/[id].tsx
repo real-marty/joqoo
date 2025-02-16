16a5ff0c1d1614780d43f62af0302fe4243ade27
Marty <riedlmarty@gmail.com>
2025-02-16 23:24:00 +0100

    Refactor PropertyDetails component for enhanced UI and functionality

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,14 +1,296 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+  Platform,
+} from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
 
-const PropertyDetails = () => {
-  const { id } = useLocalSearchParams();
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import Comment from "@/components/comment";
+import { facilities } from "@/constants/data";
+
+import { useAppwrite } from "@/hooks/use-appwrite";
+import { getPropertyById } from "@/lib/appwrite";
+
+const Property = () => {
+  const { id } = useLocalSearchParams<{ id?: string }>();
+
+  const windowHeight = Dimensions.get("window").height;
+
+  const { data: property } = useAppwrite({
+    fn: getPropertyById,
+    params: {
+      id: id!,
+    },
+  });
 
   return (
     <View>
-      <Text>PropertyDetails</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 bg-white"
+      >
+        <View className="relative w-full" style={{ height: windowHeight / 2 }}>
+          <Image
+            source={{ uri: property?.image }}
+            className="size-full"
+            resizeMode="cover"
+          />
+          <Image
+            source={images.whiteGradient}
+            className="absolute top-0 w-full z-40"
+          />
+
+          <View
+            className="z-50 absolute inset-x-7"
+            style={{
+              top: Platform.OS === "ios" ? 70 : 20,
+            }}
+          >
+            <View className="flex flex-row items-center w-full justify-between">
+              <TouchableOpacity
+                onPress={() => router.back()}
+                className="flex flex-row bg-primary-500 rounded-full size-11 items-center justify-center"
+              >
+                <Image
+                  source={icons.backArrow}
+                  className="size-5"
+                  tintColor="#ffffff"
+                />
+              </TouchableOpacity>
+
+              <View className="flex flex-row items-center gap-3">
+                <Image
+                  source={icons.heart}
+                  className="size-7"
+                  tintColor={"#191D31"}
+                />
+                <Image source={icons.send} className="size-7" />
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View className="px-5 mt-7 flex gap-2">
+          <Text className="text-2xl font-quicksand-bold">{property?.name}</Text>
+
+          <View className="flex flex-row items-center gap-3">
+            <View className="flex flex-row items-center px-4 py-2 bg-primary-500 rounded-full">
+              <Text className="text-xs font-quicksand-bold text-primary-50">
+                {property?.type}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center gap-2">
+              <Image source={icons.star} className="size-5" />
+              <Text className="text-zinc-700 text-sm mt-1 font-quicksand-medium">
+                {property?.rating} ({property?.reviews.length} Hodnocení)
+              </Text>
+            </View>
+          </View>
+
+          <View className="flex flex-row items-center mt-5">
+            <View className="flex flex-row items-center justify-center bg-primary-500 rounded-full size-10">
+              <Image
+                source={icons.bed}
+                className="size-6"
+                tintColor="#fde8eb"
+              />
+            </View>
+            <Text className="text-black text-sm font-quicksand-medium ml-2">
+              {property?.bedrooms} Beds
+            </Text>
+            <View className="flex flex-row items-center justify-center bg-primary-500 rounded-full size-10 ml-7">
+              <Image
+                source={icons.bath}
+                className="size-6"
+                tintColor="#fde8eb"
+              />
+            </View>
+            <Text className="text-black text-sm font-quicksand-medium ml-2">
+              {property?.bathrooms} Baths
+            </Text>
+            <View className="flex flex-row items-center justify-center bg-primary-500 rounded-full size-10 ml-7">
+              <Image
+                source={icons.area}
+                className="size-6"
+                tintColor="#fde8eb"
+              />
+            </View>
+            <Text className="text-black text-sm font-quicksand-medium ml-2">
+              {property?.area} sqft
+            </Text>
+          </View>
+
+          <View className="w-full border-t border-primary-300 pt-7 mt-5">
+            <Text className="text-black text-xl font-quicksand-bold">
+              Agent
+            </Text>
+
+            <View className="flex flex-row items-center justify-between mt-4">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={{ uri: property?.agent.avatar }}
+                  className="size-14 rounded-full"
+                />
+
+                <View className="flex flex-col items-start justify-center ml-3">
+                  <Text className="text-lg text-black text-start font-quicksand-bold">
+                    {property?.agent.name}
+                  </Text>
+                  <Text className="text-sm text-zinc-700 text-start font-quicksand-medium">
+                    {property?.agent.email}
+                  </Text>
+                </View>
+              </View>
+
+              <View className="flex flex-row items-center gap-3">
+                <Image source={icons.chat} className="size-7" />
+                <Image source={icons.phone} className="size-7" />
+              </View>
+            </View>
+          </View>
+
+          <View className="mt-7">
+            <Text className="text-black text-xl font-quicksand-bold">
+              Overview
+            </Text>
+            <Text className="text-zinc-700 text-base font-quicksand mt-2">
+              {property?.description}
+            </Text>
+          </View>
+
+          <View className="mt-7">
+            <Text className="text-black text-xl font-quicksand-bold">
+              Facilities
+            </Text>
+
+            {property?.facilities.length > 0 && (
+              <View className="flex flex-row flex-wrap items-start justify-start mt-2 gap-5">
+                {property?.facilities.map((item: string, index: number) => {
+                  const facility = facilities.find(
+                    (facility) => facility.title === item,
+                  );
+
+                  return (
+                    <View
+                      key={index}
+                      className="flex flex-1 flex-col items-center min-w-16 max-w-20"
+                    >
+                      <View className="size-14 bg-primary-100 rounded-full flex items-center justify-center">
+                        <Image
+                          source={facility ? facility.icon : icons.info}
+                          className="size-6"
+                        />
+                      </View>
+
+                      <Text
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        className="text-black text-sm text-center font-quicksand mt-1.5"
+                      >
+                        {item}
+                      </Text>
+                    </View>
+                  );
+                })}
+              </View>
+            )}
+          </View>
+
+          {property?.gallery.length > 0 && (
+            <View className="mt-7">
+              <Text className="text-black text-xl font-quicksand-bold">
+                Gallery
+              </Text>
+              <FlatList
+                contentContainerStyle={{ paddingRight: 20 }}
+                data={property?.gallery}
+                keyExtractor={(item) => item.$id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <Image
+                    source={{ uri: item.image }}
+                    className="size-40 rounded-xl"
+                  />
+                )}
+                contentContainerClassName="flex gap-4 mt-3"
+              />
+            </View>
+          )}
+
+          <View className="mt-7">
+            <Text className="text-black text-xl font-quicksand-bold">
+              Location
+            </Text>
+            <View className="flex flex-row items-center justify-start mt-4 gap-2">
+              <Image source={icons.location} className="w-7 h-7" />
+              <Text className="text-zinc-700 text-sm font-quicksand-medium">
+                {property?.address}
+              </Text>
+            </View>
+
+            <Image
+              source={images.map}
+              className="h-52 w-full mt-5 rounded-xl"
+            />
+          </View>
+
+          {property?.reviews.length > 0 && (
+            <View className="mt-7">
+              <View className="flex flex-row items-center justify-between">
+                <View className="flex flex-row items-center">
+                  <Image source={icons.star} className="size-6" />
+                  <Text className="text-black text-xl font-quicksand-bold ml-2">
+                    {property?.rating} ({property?.reviews.length} reviews)
+                  </Text>
+                </View>
+
+                <TouchableOpacity>
+                  <Text className="text-primary-300 text-base font-quicksand-bold">
+                    View All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <View className="mt-5">
+                <Comment item={property?.reviews[0]} />
+              </View>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+
+      <View className="absolute bg-white bottom-0 w-full rounded-t-2xl border-t border-r border-l border-primary-200 p-7">
+        <View className="flex flex-row items-center justify-between gap-10">
+          <View className="flex flex-col items-start">
+            <Text className="text-zinc-700 text-xs font-quicksand-medium">
+              Cena
+            </Text>
+            <Text
+              numberOfLines={1}
+              className="text-primary-500 text-start text-2xl font-quicksand-bold"
+            >
+              {property?.price}€
+            </Text>
+          </View>
+
+          <TouchableOpacity className="flex-1 flex flex-row items-center justify-center bg-primary-500 py-3 rounded-full shadow-md shadow-zinc-400">
+            <Text className="text-white text-lg text-center font-quicksand-bold">
+              Book Now
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
-export default PropertyDetails;
+export default Property;
