8aaa543ebadefdcf417f4f5d537e8d11d629270e
Marty <riedlmarty@gmail.com>
2025-02-16 23:24:17 +0100

    Refactor Filters component styles for improved visual consistency and typography

--- components/filters.tsx
+++ components/filters.tsx
@@ -33,15 +33,15 @@ const Filters = () => {
           key={item.title + index}
           className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${
             selectedCategory === item.category
-              ? "bg-primary-300"
-              : "bg-primary-100 border border-primary-200"
+              ? "bg-primary-500 border-primary-500"
+              : "bg-primary-50 border border-primary-500"
           }`}
         >
           <Text
             className={`text-sm ${
               selectedCategory === item.category
-                ? "text-white font-rubik-bold mt-0.5"
-                : "text-black-300 font-rubik"
+                ? "text-white font-quicksand-bold mt-0.5"
+                : "text-black font-quicksand-semibold"
             }`}
           >
             {item.title}
